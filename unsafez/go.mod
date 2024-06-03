6498111520177bfd60debf14717fc2f4cf7204f6
Oliver N <olvrng@gmail.com>
2024-06-03 13:39:37 +0700

    add unsafez

---
+++ unsafez/go.mod
@@ -0,0 +1,3 @@
+module github.com/ezpkg/unsafez
+
+go 1.21
---
+++ unsafez/unsafez.go
@@ -0,0 +1,9 @@
+package unsafez
+
+import (
+	"unsafe"
+)
+
+func BytesToString(b []byte) string {
+	return *(*string)(unsafe.Pointer(&b))
+}
