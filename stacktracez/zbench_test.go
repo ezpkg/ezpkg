184bf1c6d9823ba5ca0005d10bf0bfa5c1ae4bf4
Oliver N <olvrng@gmail.com>
2024-06-06 18:30:37 +0700

    stacktracez: update benchmark

--- stacktracez/zbench_test.go
+++ stacktracez/zbench_test.go
@@ -9,16 +9,43 @@ func BenchmarkStd(b *testing.B) {
 	b.ReportAllocs()
 	var frames *runtime.Frames
 	for i := 0; i < b.N; i++ {
-		var callers [32]uintptr
-		frames = runtime.CallersFrames(callers[:])
-		frames.Next()
+		var pc [32]uintptr
+		runtime.Callers(2, pc[:])
+		frames = runtime.CallersFrames(pc[:])
+	}
+	b.StopTimer()
+	frames.Next()
+}
+
+func BenchmarkStdNext(b *testing.B) {
+	b.ReportAllocs()
+	var frames *runtime.Frames
+	for i := 0; i < b.N; i++ {
+		var pc [32]uintptr
+		runtime.Callers(2, pc[:])
+		frames = runtime.CallersFrames(pc[:])
+		_, more := frames.Next()
+		for more {
+			_, more = frames.Next()
+		}
 	}
 }
 
 func BenchmarkZ(b *testing.B) {
 	b.ReportAllocs()
+	var frames *Frames
 	for i := 0; i < b.N; i++ {
-		frames := StackTrace()
+		frames = StackTrace()
+	}
+	b.StopTimer()
+	frames.GetFrames()
+}
+
+func BenchmarkZGetFrames(b *testing.B) {
+	b.ReportAllocs()
+	var frames *Frames
+	for i := 0; i < b.N; i++ {
+		frames = StackTrace()
 		frames.GetFrames()
 	}
 }
