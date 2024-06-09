e38ba8e5ac315fd918f7b357cdacd21948eeeaa2
Oliver N <olvrng@gmail.com>
2024-06-09 12:42:29 +0700

    colorz: Format -> Wrap

--- colorz/colorz.go
+++ colorz/colorz.go
@@ -32,7 +32,7 @@ func initCodes() (codes [_N]string) {
 }
 
 func (c Color) String() string {
-	return fmt.Sprintf("Color(%d)", byte(c))
+	return c.Code()
 }
 
 func (c Color) Code() string {
@@ -45,18 +45,28 @@ func (c Color) Code() string {
 	return ""
 }
 
-func (c Color) Format(s string) string {
-	return fmt.Sprintf("\x1b[%dm%s\x1b[0m", byte(c), s)
+func (c Color) Wrap(s string) string {
+	return c.Code() + s + Reset.Code()
 }
 
-func (c Color) Formatf(format string, args ...any) string {
-	s := fmt.Sprintf(format, args...)
-	return c.Format(s)
+func (c Color) Format(s fmt.State, verb rune) {
+	switch {
+	case verb == 'v' && (s.Flag('+') || s.Flag('#')):
+		fprintf(s, "Color(%d)", byte(c))
+	case verb == 'd':
+		fprintf(s, "%d", byte(c))
+	default:
+		writeString(s, c.Code())
+	}
+}
+
+func (c Color) Sprint(args ...any) string {
+	return c.Wrap(fmt.Sprint(args...))
 }
 
 func (c Color) Sprintf(format string, args ...any) string {
 	s := fmt.Sprintf(format, args...)
-	return c.Format(s)
+	return c.Sprint(s)
 }
 
 func (c Color) Fprintf(w io.Writer, format string, args ...any) (n int, err error) {
@@ -90,4 +100,12 @@ func (c Color) Println(args ...any) {
 	fmt.Printf("\x1b[%dm", byte(c))
 	fmt.Println(args...)
 	fmt.Printf("\x1b[0m")
+}
+
+func writeString(w fmt.State, s string) {
+	_, _ = io.WriteString(w, s)
+}
+
+func fprintf(w fmt.State, format string, args ...any) {
+	_, _ = fmt.Fprintf(w, format, args...)
 }
