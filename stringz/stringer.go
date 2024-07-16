1fbadf4102acc299f2c941c099f5ed0f1525508c
Oliver N <olvrng@gmail.com>
2024-07-16 17:57:44 +0700

    stringz: add BuilderFunc

--- stringz/stringer.go
+++ stringz/stringer.go
@@ -15,3 +15,17 @@ var _ fmt.Stringer = StringFunc(nil)
 type StringFunc func() string
 
 func (fn StringFunc) String() string { return fn() }
+
+// StringFunc wraps a function to make it implement fmt.Stringer. It is useful when you want to delay evaluation of the function. The function receives a stringz.Builder. For example:
+//
+//	msg := BuilderFunc(func(b *stringz.Builder) string {
+//	  b.Printf("JSON(obj)=%v", formatJSON(obj))
+//	})
+//	log.Debug(msg)  // 👈 only run formatJSON() if debug level is enabled
+type BuilderFunc func(b *Builder)
+
+func (fn BuilderFunc) String() string {
+	var b Builder
+	fn(&b)
+	return b.String()
+}
