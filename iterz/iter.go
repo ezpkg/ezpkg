db3c0bea1f79b35ef43d1b4e1f150ec016bc5504
Oliver N <olvrng@gmail.com>
2024-09-07 12:34:27 +0700

    ✨ iterz: Nil and Nil2

--- iterz/iter.go
+++ iterz/iter.go
@@ -1,1 +1,15 @@
 package iterz // import "ezpkg.io/iterz"
+
+import (
+	"iter"
+)
+
+// Nil return an iter.Seq that yields nothing.
+func Nil[V any]() iter.Seq[V] {
+	return func(yield func(V) bool) {}
+}
+
+// Nil2 return an iter.Seq2 that yields nothing.
+func Nil2[K, V any]() iter.Seq2[K, V] {
+	return func(yield func(K, V) bool) {}
+}
